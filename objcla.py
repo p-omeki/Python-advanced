d7e3f8e6fe3dd091a6436c72ebb89832e3f3e04e
unknown <partymeki@gmail.com>
2023-05-10 21:42:25 +0300

    Updated Class

--- objcla.py
+++ objcla.py
@@ -1,1 +1,12 @@
 #In this Python Objects and Class, the focus is on becoming an expert in Python's Object Oriented Programming.
+#Creating a class in python
+"""
+    class ClassName:
+    'Simple documentation of class string'
+    class_suite
+    
+    """
+    
+'''The code above, the statement "class" initiates a class followed by a class name tagged "ClassName" and ecnlosed with a (:).
+    The class_suite consist of all the component statement defining class members, attributes and functions.
+    '''
